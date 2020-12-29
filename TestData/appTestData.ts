6dfb07a6442595bf08cadefd316519f269c2862a
SaniyaBhaldar <saniya.bhaldar96@gmail.com>
2020-12-29 14:06:12 +0530

    Added test data

--- TestData/appTestData.ts
+++ TestData/appTestData.ts
@@ -1,11 +1,17 @@
 export default {
     appTestData: {
+
+        baseURLData:{
+            baseURL: "http://petclinicui.e46708b92c054086909b.eastus.aksapp.io/petclinic/",
+        },
+
         newOwnerData: {
             FirstName: "Raj",
             LastName: "Mane",
             Address: "15 street",
             City: "NewYork",
-            Telephone: "9870688977"
+            Telephone: "9870688977",
+            expectedData: "Raj Mane 15 street NewYork 9870688977"
         },
 
         petDetails: {
@@ -13,6 +19,8 @@ export default {
             PetBirthdate: "2010/01/20",
             PetType: "snake",
         }
+
+
 
 
     }
